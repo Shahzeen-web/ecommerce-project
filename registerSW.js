d6350d07da5626a6beeae748880fa6efa0c83268
Shahzeen <shahzeen609@example.com>
2025-08-26 13:22:26 +0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ecommerce-project/sw.js', { scope: '/ecommerce-project/' })})}
